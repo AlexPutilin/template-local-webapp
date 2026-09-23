9777c15745e3ca2b002d97ace35ab3e3d3b7cebe
Alexander Putilin <alexander.putilin@yahoo.de>
2026-09-23 14:49:30 +0200

    feat: add scheduler in the application root.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,49 @@
 import config from '../app.config.json' with { type: 'json' };
 import createHttpServer from '#framework/http/http.js';
-import pageRoutes from '#application/routes/pages.js';
+import createTaskScheduler from '#framework/scheduler/taskScheduler.js';
+import userRoutes from '#application/routes/userRoutes.js';
+import pageRoutes from '#application/routes/pageRoutes.js';
 
 
-const httpServer = createHttpServer(router => {
+const taskScheduler = createTaskScheduler((scheduler) => {
+    // Register your tasks here
+});
+
+
+const httpServer = createHttpServer((router) => {
     router.register('/', pageRoutes);
+    router.register('/api/users', userRoutes);
 });
 
 
 httpServer.on('error', (err) => {
     console.error('HTTP-Server failed to start:', err);
+    taskScheduler.stop();
     process.exit(1);
 });
 
 
 httpServer.on('listening', () => {
-    console.log(`HTTP-Server is listening on http://${config.http.host}:${config.http.port}`);
+    console.log(`HTTP-Server is listening`);
+    taskScheduler.start();
 });
+
+
+function shutdown(signal) {
+    console.log(`Received ${signal}. Stopping application.`);
+    taskScheduler.stop();
+    httpServer.close((error) => {
+        if (error) {
+            console.error('Error occurred while closing HTTP-Server:', error);
+            process.exit(1);
+        }
+        process.exit(0);
+    });
+}
+
+
+process.once('SIGINT', () => shutdown('SIGINT'));
+process.once('SIGTERM', () => shutdown('SIGTERM'));
 
 
 httpServer.listen(config.http.port, config.http.host);
